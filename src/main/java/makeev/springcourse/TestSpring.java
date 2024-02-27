a14e11b638365aa2a3aa576dc4b542bb80aae360
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-02-27 09:25:03 +0300

    Dependency Injection with Spring with XML

--- src/main/java/makeev/springcourse/TestSpring.java
+++ src/main/java/makeev/springcourse/TestSpring.java
@@ -7,8 +7,9 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        System.out.println(music.getSong());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer.playMusic();
+
         context.close();
     }
 
