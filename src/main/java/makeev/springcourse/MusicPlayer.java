5a518a24cad59b50d148675358f0b2b12c39084e
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-02-27 11:38:54 +0300

    Dependency Injection with setter with XML and properties

--- src/main/java/makeev/springcourse/MusicPlayer.java
+++ src/main/java/makeev/springcourse/MusicPlayer.java
@@ -4,9 +4,29 @@ public class MusicPlayer {
 
     private Music music;
 
-    public MusicPlayer(Music music) {
+    private String name;
+    private int volume;
+
+    public void setMusic(Music music) {
         this.music = music;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
--- src/main/java/makeev/springcourse/UseMusicPlayer.java
+++
@@ -1,7 +0,0 @@
-package makeev.springcourse;
-
-public class UseMusicPlayer {
-    public static void main(String[] args) {
-        MusicPlayer musicPlayer = new MusicPlayer(new RockMusic());
-    }
-}
